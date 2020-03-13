f798c23ce9663f63f3c7cb36660659f88cb956b2
Carla Cauche <58265171+carlacch@users.noreply.github.com>
2020-03-13 14:34:01 +0100

    update fragment with bottomNavigationBar

--- app/src/main/java/fr/esilv/howtocook/MainActivity.java
+++ app/src/main/java/fr/esilv/howtocook/MainActivity.java
@@ -1,23 +1,52 @@
 package fr.esilv.howtocook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     //private EditText searchBar;
     //private Button searchBtn;
+    Fragment frag_menu = new MenuFragment();
+
+    private BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener itemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.home :
+                    showFragment(frag_menu);
+                    break;
+                case R.id.starter :
+                    break;
+                case R.id.main_courses :
+                    break;
+                case R.id.desserts :
+                    break;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        showFragment(new MenuFragment());
+        bottomNavigationView.setOnNavigationItemSelectedListener(itemSelectedListener);
 
         /*
         searchBar = findViewById(R.id.main_activity_search_bar);
@@ -29,7 +58,12 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("QUERY", query);
             startActivity(intent);
         });*/
+    }
 
-
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.Main_frameLayout, fragment)
+                .commit();
     }
 }
